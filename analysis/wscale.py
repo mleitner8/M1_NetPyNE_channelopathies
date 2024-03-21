e6d5612af5ec30883c71b4f98e8b11fd76244a1b
Roman Baravalle <44242643+RomanB22@users.noreply.github.com>
2024-03-21 14:47:03 -0500

    wscale.py updated
    Adding EPSPs plotting (commented in the code) and showing the way to unify data in a single allData.json the first time you run the script

--- analysis/wscale.py
+++ analysis/wscale.py
@@ -1,3 +1,4 @@
+
 """
 wscale.py
 
@@ -12,16 +13,18 @@ import numpy as np
 from scipy.interpolate import interp1d
 from cycler import cycler
 import sys, os
+import time
+#import termplotlib as tpl
+#import plotext as plt
 
 def calculateEPSPs(params, data, somaLabel='soma', stimRange=[3000,4000], syn='exc'):
     out = {}
     secs = [s for s in params[0]['values']]
     locs = [s for s in params[1]['values']]
 
-    for key, d in data.items(): #changed iteritems to items (python version change)
+    for key, d in data.items(): #changed iteritems to items
         #cellLabel = d['data']['V_soma'].keys()[0]
-        vsoma = d['V_'+somaLabel]['cell_0']
-        #vsoma = d['data']['V_'+somaLabel][cellLabel]
+        vsoma = d['V_'+somaLabel]['cell_0'] #vsoma = d['data']['V_'+somaLabel][cellLabel]
         if syn == 'exc':
             epsp = max(vsoma[stimRange[0]:stimRange[1]]) - vsoma[stimRange[0]-1] # max voltage between stim time - baseline
         elif syn == 'inh':
@@ -46,9 +49,8 @@ def calculateEPSPsPops(params, data, somaLabel='soma', stimRange=[3000,4000], sy
         for sec,loc in zip(secs,locs): out[pop][(sec,loc)] = []
 
     for key, d in data.items(): #change iteritems to items
-        #cellLabel = d['simData']['V_soma'].keys()[0] #d['V_soma'].keys()[0]
-        vsoma = d['V_'+somaLabel]['cell_0']
-        #d['simData']['V_'+somaLabel][cellLabel]
+        #cellLabel = d['V_soma'].keys()[0] # d['simData']['V_soma'].keys()[0]
+        vsoma = d['V_'+somaLabel]['cell_0']  #d['simData']['V_'+somaLabel][cellLabel]
         if syn == 'exc':
             epsp = max(vsoma[stimRange[0]:stimRange[1]]) - vsoma[stimRange[0]-1] # max voltage between stim time - baseline
         elif syn == 'inh':
@@ -115,6 +117,25 @@ def calculateWeightNormPops(params, data, epspNorm=0.5, somaLabel='soma', stimRa
             print(wnorm)
             weightNorm[pop][seg[0]].append(wnorm)
             print('\n%s %s wscale = %.6f' % (pop, str(seg), wnorm))
+            '''Plot EPSPs
+            #if wnorm <=0:
+            #    jj = f(x)
+            #    plt.scatter(x, y)
+                #plt.plot(x, f(x))
+            #    plt.xlabel('Stimulation Intensity')
+            #    plt.ylabel('EPSP')
+            #    plt.show()
+            #    quit()
+            #jj = f(x)
+            plt.figure()
+            plt.scatter(x, y)
+            #plt.plot(x, jj)
+            plt.xlabel('Stimulation Intensity')
+            plt.ylabel('EPSP')
+            plt.savefig('plots/'+str(pop)+'_'+str(seg)+'.png')
+            plt.close()
+            '''
+
 
         if savePath:
             import pickle
@@ -170,8 +191,8 @@ if __name__ == '__main__':
 
     # run batch E cells
     
-    dataFolder = '../data/'
-    batchLabel = 'batch_2024-02-22'   # v52_batch3'
+    dataFolder = '../data'
+    batchLabel = 'wscale'   # v52_batch3'
     #loadFromFile = True
 
     ''' run via batch.py
@@ -184,13 +205,15 @@ if __name__ == '__main__':
     '''
 
     # analyze batch E cells    
-    params, data = utils.readBatchData(dataFolder, batchLabel, loadAll=False, saveAll=True, vars=[('simData', 'V_soma')], maxCombs=None)
-    #epsp = calculateEPSPs(params, data, somaLabel = 'soma', stimRange = [3000, 4000], syn = 'exc')
-    epsp = calculateEPSPsPops(params, data, somaLabel='soma', stimRange=[10*700,10*800], syn='exc')
+    #params, data = utils.readBatchData(dataFolder,batchLabel,vars=[('simData', 'V_soma')],loadAll=True,saveAll=True,maxCombs=None)
+    #FIRST TIME YOU RUN, YOU NEED TO PUT LOADALL=FALSE
+    params, data = utils.readBatchData(dataFolder, batchLabel, loadAll=False, saveAll=True, vars=[('simData', 'V_soma')], maxCombs=None)    
+    #epsp = calculateEPSPsPops(params, data, somaLabel='soma', stimRange=[10*700,10*800], syn='exc')
     #plotEPSPs(epsp, dataFolder, batchLabel, addLegend=0)
     #plotEPSPs(epsp, dataFolder, batchLabel, addLegend=1, includeSegs=[('apic_28',0.5), ('apic_36',0.5), ('apic_49',0.5), ('apic_56',0.5)])
-    #calculateWeightNorm(params, data, epspNorm = 0.5, somaLabel = 'soma', stimRange=[3000,4000], savePath=True)
-
-    calculateWeightNormPops(params, data,  somaLabel='soma', stimRange=[10*700,10*800], savePath=dataFolder+'/'+batchLabel+'/')
+    #calculateWeightNorm(params, data, epspNorm = 0.5, somaLabel = 'soma', stimRange=[10*700,10*800], savePath=True)
+    #print(data); quit()
+    ts = int(1/0.025)
+    calculateWeightNormPops(params, data,  somaLabel='soma', stimRange=[ts*700,ts*800], savePath=dataFolder+'/'+batchLabel+'/')
 
 
