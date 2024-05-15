19ec2d7746a9bcc31c497a8a476628a9ad29af68
Molly <molly.leitner@downstate.edu>
2024-05-15 13:20:52 -0400

    wscale pop changes

--- analysis/wscale.py
+++ analysis/wscale.py
@@ -226,12 +226,12 @@ if __name__ == '__main__':
     # analyze batch E cells    
     params, data = utils.readBatchData(dataFolder, batchLabel, loadAll=True, saveAll=False, vars=[('simData', 'V_soma')], maxCombs=None)
     #epsp = calculateEPSPs(params, data, somaLabel = 'soma', stimRange = [3000, 4000], syn = 'exc')
-    #epsp = calculateEPSPsPops(params, data, somaLabel='soma', stimRange=[10*700,10*800], syn='exc')
+    epsp = calculateEPSPsPops(params, data, somaLabel='soma', stimRange=[10*700,10*800], syn='exc')
     #plotEPSPs(epsp, dataFolder, batchLabel, addLegend=0)
     #plotEPSPs(epsp, dataFolder, batchLabel, addLegend=1, includeSegs=[('apic_28',0.5), ('apic_36',0.5), ('apic_49',0.5), ('apic_56',0.5)])
-    calculateWeightNorm(params, data, epspNorm = 0.5, somaLabel = 'soma', stimRange=[10*700,10*800], savePath=True)
+    #calculateWeightNorm(params, data, epspNorm = 0.5, somaLabel = 'soma', stimRange=[10*700,10*800], savePath=True)
     #print(data); quit()
     #ts = int(1/0.025)
-    #calculateWeightNormPops(params, data,  somaLabel='soma', stimRange=[ts*700,ts*800], savePath=dataFolder+'/'+batchLabel+'/')
+    calculateWeightNormPops(params, data,  somaLabel='soma', stimRange=[ts*700,ts*800], savePath=dataFolder+'/'+batchLabel+'/')
 
 
